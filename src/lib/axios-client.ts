6ab1d7b0688eb43a23c45e4ce11936ae9e2ec110
Rashid251 <rashidhayat250@gmail.com>
2026-04-25 21:13:18 +0530

    Increase timeout for production

--- src/lib/axios-client.ts
+++ src/lib/axios-client.ts
@@ -6,7 +6,7 @@ const baseURL = import.meta.env.VITE_API_BASE_URL;
 const options = {
   baseURL,
   withCredentials: true,
-  timeout: 10000,
+  timeout: 30000,
 };
 
 const API = axios.create(options);
